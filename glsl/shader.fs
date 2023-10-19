68a0d06e68d0dd4dc3561fd01500eb827f4e274d
xxxcc <xxxcc@example.com>
2023-10-20 00:07:27 +0800

    在观察空间中计算冯氏光照 顶点着色器中，要将光源位置转换成观察坐标，同时计算输出时也要乘上view矩阵 片段着色器中，计算镜面反射光照时，观察方向的方向向量就不需要减去观察位置了，因为观察位置就是原点。

--- glsl/shader.fs
+++ glsl/shader.fs
@@ -1,13 +1,13 @@
 #version 330 core
 in vec3 FragPos;
 in vec3 Normal;
+in vec3 LightPos;
 
 out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+// uniform vec3 viewPos; 观察点变成了原点
 
 void main()
 {
@@ -20,14 +20,14 @@ void main()
     // 漫反射光照
     // --------
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(LightPos - FragPos);
 
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
     // 镜面光照
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(-FragPos); // 因为观察点是原点，因此不需要减去观察位置
     vec3 reflectDir = reflect(-lightDir, norm);
 
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
--- glsl/shader.vs
+++ glsl/shader.vs
@@ -4,6 +4,9 @@ layout (location = 1) in vec3 aNormal;
 
 out vec3 FragPos;
 out vec3 Normal;
+out vec3 LightPos;
+
+uniform vec3 lightPos; // lightPos定义在顶点着色器是因为传入的是光源的世界坐标
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,6 +15,7 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    Normal = mat3(transpose(inverse(model))) * aNormal;
-    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    FragPos = vec3(view * model * vec4(aPos, 1.0));
+    LightPos = vec3(view * vec4(lightPos, 1.0));
 }
